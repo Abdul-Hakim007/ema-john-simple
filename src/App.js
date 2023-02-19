a4dc728dd3f085d2319729d39a0b699f4211f135
Abdul-Hakim007 <abdulhakimjoydebpur@gmail.com>
2023-02-19 23:32:32 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Invantory/Inventory';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
+     
+      <Router>
       <Header></Header>
-      <Shop></Shop>
+        <Routes>
+          <Route exact path='/' element={<Shop/>}/>
+          
+          <Route path='/shop' element={<Shop />}/>
+        
+          <Route path='/inventory' element={<Inventory/>} />
+           
+        
+          <Route path='/review' element={<OrderReview/> }/>
+
+          <Route path='*' element={<NotFound/>}/>
+         
+          
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
